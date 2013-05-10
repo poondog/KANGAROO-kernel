2f5615f64769f2e790f5d563a184355df49f1f4b
markj338 <markj338@gmail.com>
2013-05-10 19:45:30 +1000

    Switch to CAF freq table, add UC/OC from 122-1804MHz

--- arch/arm/mach-msm/acpuclock-7x30.c
+++ arch/arm/mach-msm/acpuclock-7x30.c
@@ -90,13 +90,10 @@ static struct clock_state drv_state = { 0 };
 static struct clkctl_acpu_speed *backup_s;
 
 static struct pll pll2_tbl[] = {
-        {  42, 0, 1, 0 }, /*  806 MHz */
-        {  53, 1, 3, 0 }, /* 1024 MHz */
-        { 125, 0, 1, 1 }, /* 1200 MHz */
-        {  73, 0, 1, 0 }, /* 1401 MHz */
-        {  79, 0, 1, 0 }, /* 1500 MHz */
-        {  81, 0, 1, 0 }, /* 1527 MHz */
-        {  83, 0, 1, 0 }, /* 1553 MHz */
+	{  42, 0, 1, 0 }, /*  806 MHz */
+	{  53, 1, 3, 0 }, /* 1024 MHz */
+	{ 125, 0, 1, 1 }, /* 1200 MHz */
+	{  73, 0, 1, 0 }, /* 1401 MHz */
 };
 
 /* Use negative numbers for sources that can't be enabled/disabled */
@@ -120,22 +117,27 @@ static struct clk *acpuclk_sources[MAX_SOURCE];
  * know all the h/w requirements.
  */
 static struct clkctl_acpu_speed acpu_freq_tbl[] = {
-	{ 0, 24576,  LPXO,     0, 0,  30720000,  850, VDD_RAW(850) },
-	{ 0, 61440,  PLL_3,    5, 11, 61440000,  850, VDD_RAW(850) },
-	{ 0, 122880, PLL_3,    5, 5,  61440000,  850, VDD_RAW(850) },
+	{ 0, 24576,  LPXO, 0, 0,  30720000,  900, VDD_RAW(900) },
+	{ 0, 61440,  PLL_3,    5, 11, 61440000,  900, VDD_RAW(900) },
+	{ 1, 122880, PLL_3,    5, 5,  61440000,  825, VDD_RAW(825) },
 	{ 0, 184320, PLL_3,    5, 4,  61440000,  850, VDD_RAW(850) },
-	{ 0, MAX_AXI_KHZ, AXI, 1, 0,  61440000,  825, VDD_RAW(825) },
-	{ 1, 149760, PLL_3,    5, 2,  61440000,  800, VDD_RAW(800) },
-	{ 1, 245760, PLL_3,    5, 1,  122800000, 850, VDD_RAW(850) },
-	{ 1, 341640, PLL_3,    5, 1,  122800000, 900, VDD_RAW(900) },
-	{ 1, 614400, PLL_1,    2, 0,  153600000, 1000, VDD_RAW(1000) },
-	{ 1, 768000,  PLL_2, 3, 0, 192000000, 1050, VDD_RAW(1050), &pll2_tbl[0]},
-	{ 1, 806400,  PLL_2, 3, 0, 192000000, 1100, VDD_RAW(1100), &pll2_tbl[1]},
-	{ 1, 1024000, PLL_2, 3, 0, 192000000, 1200, VDD_RAW(1200), &pll2_tbl[2]},
-	{ 1, 1200000, PLL_2, 3, 0, 192000000, 1200, VDD_RAW(1200), &pll2_tbl[3]},
-	{ 1, 1305600, PLL_2, 3, 0, 192000000, 1225, VDD_RAW(1225), &pll2_tbl[4]},
-	{ 1, 1401600, PLL_2, 3, 0, 192000000, 1250, VDD_RAW(1250), &pll2_tbl[5]},
-	{ 1, 1516800, PLL_2, 3, 0, 192000000, 1300, VDD_RAW(1300), &pll2_tbl[6]},
+	{ 0, MAX_AXI_KHZ, AXI, 1, 0, 61440000, 900, VDD_RAW(900) },
+	{ 1, 245760, PLL_3,    5, 2,  61440000,  875, VDD_RAW(875) },
+	{ 1, 368640, PLL_3,    5, 1,  122800000, 900, VDD_RAW(900) },
+	/* AXI has MSMC1 implications. See above. */
+	{ 1, 768000, PLL_1,    2, 0,  153600000, 950, VDD_RAW(950) },
+	/*
+	 * AXI has MSMC1 implications. See above.
+	 */
+	{ 1, 806400,  PLL_2, 3, 0, UINT_MAX, 975, VDD_RAW(975), &pll2_tbl[0]},
+	{ 1, 1024000, PLL_2, 3, 0, UINT_MAX, 1100, VDD_RAW(1000), &pll2_tbl[1]},
+	{ 1, 1075200, PLL_2, 3, 0, UINT_MAX, 1150, VDD_RAW(1150), &pll2_tbl[2]},
+	{ 1, 1152000, PLL_2, 3, 0, UINT_MAX, 1125, VDD_RAW(1125), &pll2_tbl[3]},
+	{ 1, 1200000, PLL_2, 3, 0, UINT_MAX, 1150, VDD_RAW(1150), &pll2_tbl[4]},
+	{ 1, 1401600, PLL_2, 3, 0, UINT_MAX, 1200, VDD_RAW(1200), &pll2_tbl[5]},
+	{ 1, 1497600, PLL_2, 3, 0, UINT_MAX, 1250, VDD_RAW(1250), &pll2_tbl[6]},
+	{ 1, 1708800, PLL_2, 3, 0, UINT_MAX, 1300, VDD_RAW(1300), &pll2_tbl[7]},
+	{ 1, 1804800, PLL_2, 3, 0, UINT_MAX, 1350, VDD_RAW(1350), &pll2_tbl[8]},
 	{ 0 }
 };
 
