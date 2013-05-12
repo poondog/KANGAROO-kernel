1817213f223a90086bd36562bd74c9f6a9483ec7
markj338 <markj338@gmail.com>
2013-05-12 21:35:21 +1000

    CPUfreq: Fine tuning SmartMax to favor lower freqs

--- drivers/cpufreq/cpufreq_smartmax.c
+++ drivers/cpufreq/cpufreq_smartmax.c
@@ -52,7 +52,7 @@
  * lowering the frequency towards the ideal frequency is faster than below it.
  */
 
-#define DEFAULT_SUSPEND_IDEAL_FREQ 368000
+#define DEFAULT_SUSPEND_IDEAL_FREQ 245760
 static unsigned int suspend_ideal_freq;
 
 #define DEFAULT_AWAKE_IDEAL_FREQ 768000
@@ -77,13 +77,13 @@ static unsigned int ramp_down_step;
 /*
  * CPU freq will be increased if measured load > max_cpu_load;
  */
-#define DEFAULT_MAX_CPU_LOAD 80
+#define DEFAULT_MAX_CPU_LOAD 99
 static unsigned int max_cpu_load;
 
 /*
  * CPU freq will be decreased if measured load < min_cpu_load;
  */
-#define DEFAULT_MIN_CPU_LOAD 50
+#define DEFAULT_MIN_CPU_LOAD 80
 static unsigned int min_cpu_load;
 
 /*
@@ -108,7 +108,7 @@ static unsigned int sampling_rate;
 #define DEFAULT_INPUT_BOOST_DURATION 50000000
 static unsigned int input_boost_duration;
 
-static unsigned int touch_poke_freq = 1024000;
+static unsigned int touch_poke_freq = 768000;
 static bool touch_poke = true;
 
 /*
@@ -120,7 +120,7 @@ static bool ramp_up_during_boost = true;
  * external boost interface - boost if duration is written
  * to sysfs for boost_duration
  */
-static unsigned int boost_freq = 1024000;
+static unsigned int boost_freq = 768000;
 static bool boost = true;
 
 /* in nsecs */
