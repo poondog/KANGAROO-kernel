eed72750145db2a36d363dc8d70a7c3c38a4edd5
bsmitty2 <bsmitty2@bsmitty2-HP-Compaq-6710b-RM342UT-ABA.(none)>
2013-06-18 08:51:54 -0400

    added init vivow defconfig

---
+++ include/generated/compile.h
@@ -0,0 +1,8 @@
+/* This file is auto generated, version 1 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 PREEMPT Mon Jun 17 12:53:35 EDT 2013"
+#define LINUX_COMPILE_BY "bsmitty2"
+#define LINUX_COMPILE_HOST "bsmitty2-HP-Compaq-6710b-RM342UT-ABA"
+#define LINUX_COMPILE_DOMAIN
+#define LINUX_COMPILER "gcc version 4.7.3 20130205 (prerelease) (crosstool-NG hg+default-67df47c6ccd0 - Linaro GCC 2013.02 - Cortex-A8) "
