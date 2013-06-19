bc9a3120b166bbb9114aacb13176e5fd7d5e0d76
bsmitty2 <bsmitty2@bsmitty2-HP-Compaq-6710b-RM342UT-ABA.(none)>
2013-06-18 23:49:30 -0400

    sweep 2 wake enabled for bosrd vivow , little fixes

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 1 */
+/* This file is auto generated, version 4 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 PREEMPT Mon Jun 17 12:53:35 EDT 2013"
+#define UTS_VERSION "#4 PREEMPT Tue Jun 18 23:26:33 EDT 2013"
 #define LINUX_COMPILE_BY "bsmitty2"
 #define LINUX_COMPILE_HOST "bsmitty2-HP-Compaq-6710b-RM342UT-ABA"
 #define LINUX_COMPILE_DOMAIN
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.0.65-ck1NuSeNsE"
+#define UTS_RELEASE "3.0.65-ck1NuSeNsE-geed7275-dirty"
