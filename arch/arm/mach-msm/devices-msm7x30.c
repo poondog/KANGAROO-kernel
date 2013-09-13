336af8feb6a6878036539777668807cd3bab7a8f
markj338 <markj338@gmail.com>
2013-09-13 23:41:15 +1000

    DEVICE: patch from Chad Goodman to help prevent (gfx2d1_clk status st… …uck at off) errors

--- arch/arm/mach-msm/devices-msm7x30.c
+++ arch/arm/mach-msm/devices-msm7x30.c
@@ -1201,7 +1201,7 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 	.num_levels = 3,
 	.set_grp_async = set_grp3d_async,
 	.idle_timeout = HZ/20,
-	.nap_allowed = true,
+	.nap_allowed = false,
 	.idle_needed = true,
 	.clk_map = KGSL_CLK_SRC | KGSL_CLK_CORE |
 		KGSL_CLK_IFACE | KGSL_CLK_MEM,
@@ -1244,7 +1244,7 @@ static struct kgsl_device_platform_data kgsl_2d0_pdata = {
 	/* HW workaround, run Z180 SYNC @ 192 MHZ */
 	.set_grp_async = NULL,
 	.idle_timeout = HZ/10,
-	.nap_allowed = true,
+	.nap_allowed = false,
 	.idle_needed = true,
 	.clk_map = KGSL_CLK_CORE | KGSL_CLK_IFACE,
 };
