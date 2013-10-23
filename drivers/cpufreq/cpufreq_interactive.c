d956403c69c84ef43594b0244c73a9850401b6a9
markj338 <markj338@gmail.com>
2013-10-23 12:25:50 +1100

    CPUFREQ: define default_hispeed_freq as 1024MHz

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -63,7 +63,8 @@ static spinlock_t speedchange_cpumask_lock;
 static struct mutex gov_lock;
 
 /* Hi speed to bump to from lo speed when load burst (default max) */
-static unsigned int hispeed_freq;
+#define DEFAULT_HISPEED_FREQ 1024000
+static u64 hispeed_freq;
 
 /* Go to hi speed when CPU load at or above this value. */
 #define DEFAULT_GO_HISPEED_LOAD 99
